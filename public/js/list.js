a259580e924bf529e2307acc7ed941316edc0061
doanhnd <doanhbk1994@gmail.com>
2023-05-31 01:25:02 +0900

    test

--- public/js/list.js
+++ public/js/list.js
@@ -1,8 +1,14 @@
 
 
 $(function() {
-    $(document).on('click', '#list_job figure', function(e) {
+    $(document).on('click', '#list_job figure', '' , function(e) {
         openLoading();
         window.location.href = $(this).data('url');
+    });
+
+    $(document).on('click', '#list_job .detailBtn ', function(e) {
+        e.preventDefault();
+        openLoading();
+        window.location.href = $(this).attr('href');
     });
 });
